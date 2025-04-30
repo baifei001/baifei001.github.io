eb12338a443346c92c9c6ba14850443bd79cc795
baifei001 <3209210984@qq.com>
2025-04-30 23:24:24 +0800

    修改页脚

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -1,26 +1,33 @@
 <script lang="ts" setup>
-import { useScriptTag } from '@vueuse/core'
+import {useScriptTag} from '@vueuse/core'
 import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
 
 useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
 </script>
 
 
-
 <template>
-    <YunFooter>
-      <div>
-        <VCLiveTime start-time="2025-04-26">
-          <template #live-time-before>
-            <span>本站已运行</span>
-          </template>
-          <template #live-time-after>
-            <span>后缀</span>
-          </template>
-        </VCLiveTime>
-        <!-- 自定义页脚内容 -->
-        <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
-        <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
+  <YunFooter>
+    <div class="flex justify-center gap-6 flex-wrap">
+      <VCLiveTime start-time="2025-04-26" class="flex items-center gap-2">
+        <template #live-time-before>
+          <span class="mr-1">🏃♀️本站已运行</span>
+        </template>
+        <template #live-time-after>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        </template>
+      </VCLiveTime>
+
+      <div class="flex items-center gap-4 text-gray-500">
+        <div>🌐 <span id="busuanzi_value_site_pv"/>次访问</div>
+        <div class="h-4 w-px bg-gray-200"></div> <!-- 竖线分隔 -->
+        <div>👥 <span id="busuanzi_value_site_uv"/>位访客</div>
       </div>
+    </div>
   </YunFooter>
 </template>
+
+<style>
+
+
+</style>
