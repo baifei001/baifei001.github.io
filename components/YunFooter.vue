5c6bdbc1fa55239d0603572568f9d40f7ccb600a
JZH <3209210984@qq.com>
2026-01-11 19:40:29 +0800

    测试不蒜子

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -1,5 +1,33 @@
-<script lang="ts" setup xmlns="http://www.w3.org/1999/html">
-import {useScriptTag} from '@vueuse/core'
+<!--<script lang="ts" setup xmlns="http://www.w3.org/1999/html">-->
+<!--import {useScriptTag} from '@vueuse/core'-->
+<!--import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'-->
+
+<!--useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <YunFooter>-->
+<!--    <div class="flex justify-center flex-wrap z-index-0">-->
+<!--      <VCLiveTime start-time="2025-04-26">-->
+<!--        <template #live-time-before>-->
+<!--          <span>🏃♀️本站已运行</span>-->
+<!--        </template>-->
+<!--        <template #live-time-after>-->
+<!--          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>-->
+<!--        </template>-->
+<!--      </VCLiveTime>-->
+
+<!--      <div class="flex items-center">-->
+<!--        <div>&nbsp;&nbsp;🌐 <span id="busuanzi_value_site_pv"/> 次访问</div>-->
+<!--        <div>&nbsp;&nbsp;👥 <span id="busuanzi_value_site_uv"/> 位访客</div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </YunFooter>-->
+<!--</template>-->
+
+
+<script lang="ts" setup>
+import { useScriptTag } from '@vueuse/core'
 import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
 
 useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
@@ -7,20 +35,8 @@ useScriptTag('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
 
 <template>
   <YunFooter>
-    <div class="flex justify-center flex-wrap z-index-0">
-      <VCLiveTime start-time="2025-04-26">
-        <template #live-time-before>
-          <span>🏃♀️本站已运行</span>
-        </template>
-        <template #live-time-after>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        </template>
-      </VCLiveTime>
-
-      <div class="flex items-center">
-        <div>&nbsp;&nbsp;🌐 <span id="busuanzi_value_site_pv"/> 次访问</div>
-        <div>&nbsp;&nbsp;👥 <span id="busuanzi_value_site_uv"/> 位访客</div>
-      </div>
-    </div>
+    <!-- 自定义页脚内容 -->
+    <div>本站总访问量 <span id="busuanzi_value_site_pv" /> 次</div>
+    <div>本站访客数 <span id="busuanzi_value_site_uv" /> 人次</div>
   </YunFooter>
 </template>
