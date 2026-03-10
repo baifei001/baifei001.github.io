637315453f54029ebb95caf7e09cdc61ba5f0610
baifei001 <3209210984@qq.com>
2026-03-10 13:16:46 +0800

    docs(posts): 删除明道云学习HAP相关文档
    - 移除HAP学习笔记的markdown文件
    - 更新路由配置删除HAP页面路由记录

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -30,7 +30,6 @@ declare module 'vue-router/auto-routes' {
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
     '/posts/': RouteRecordInfo<'/posts/', '/posts', Record<never, never>, Record<never, never>>,
     '/posts/Flutter': RouteRecordInfo<'/posts/Flutter', '/posts/Flutter', Record<never, never>, Record<never, never>>,
-    '/posts/HAP': RouteRecordInfo<'/posts/HAP', '/posts/HAP', Record<never, never>, Record<never, never>>,
     '/posts/hello-valaxy': RouteRecordInfo<'/posts/hello-valaxy', '/posts/hello-valaxy', Record<never, never>, Record<never, never>>,
     '/posts/idea-plugin': RouteRecordInfo<'/posts/idea-plugin', '/posts/idea-plugin', Record<never, never>, Record<never, never>>,
     '/posts/in18': RouteRecordInfo<'/posts/in18', '/posts/in18', Record<never, never>, Record<never, never>>,
